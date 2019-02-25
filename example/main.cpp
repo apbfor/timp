c1bd50d2a1bedc3f01b5df48b9aaa17c142cb28c
apbfor <apbfor@1.ru>
2019-02-25 13:28:32 +0300

    hmm

---
+++ example/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by user on 2/25/19.
+//
+
+#include <iostream>
+#include "example.h"
+
+int main(int argc, char*argv[])
+{
+    std::cout << "Hellp world!" << std::endl << sum(12,7) << std::endl;
+    return 0;
+}
