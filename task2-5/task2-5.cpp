d760788f50c5f8e3364688da877711c36283c34c
apbfor <apbforgame@gmail.com>
2019-05-05 18:12:17 +0300

    pz-5 added files

---
+++ task2-5/task2-5.cpp
@@ -0,0 +1,5 @@
+//
+// Created by apbfor on 05.05.19.
+//
+
+#include "task2-5.h"
---
+++ task2-5/task2-5.h
@@ -0,0 +1,8 @@
+//
+// Created by apbfor on 05.05.19.
+//
+
+#ifndef TEST_EMPTY_TASK2_5_H
+#define TEST_EMPTY_TASK2_5_H
+
+#endif //TEST_EMPTY_TASK2_5_H
