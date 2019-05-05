9897cb47e08e4b81f43407a1404c6a1b753d65ba
apbfor <apbforgame@gmail.com>
2019-05-05 21:04:09 +0300

    eng comments

--- task2-5/task2-5_main.cpp
+++ task2-5/task2-5_main.cpp
@@ -2,3 +2,15 @@
 // Created by apbfor on 05.05.19.
 //
 
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    for(int i=0; i<=26; i++){
+        if((i%12)+1==12)
+            cout << i <<' ';
+    }
+
+    return 0;
+}
