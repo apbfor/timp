47836d06ea1e7561f15e17188f3b74f44991aa0d
apbfor <apbforgame@gmail.com>
2019-05-12 17:36:15 +0300

    up

--- example/example_test.cpp
+++ example/example_test.cpp
@@ -3,15 +3,13 @@
 
 #include "example.h"
 
+// внимание! define CATCH_CONFIG_MAIN должен быть выше include  "../catch.hpp"
+// иначе будет ошибка вида
+// .../tests.cpp:23: undefined reference to Catch::StringRef::StringRef(char const*)
 #define CATCH_CONFIG_MAIN
 #include "../catch.hpp"
 
 TEST_CASE("example", "example")
 {
 	REQUIRE(sum(5, 10) == 15);
-}
-
-TEST_CASE("example1","example1")
-{
-    REQUIRE(sum(5,12)==10);
 }
