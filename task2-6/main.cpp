0396b32b2683bb59e08f8c5a1e3db92991fbf8cd
apbfor <apbforgame@gmail.com>
2019-05-19 09:49:43 +0300

    make as class Graph

--- task2-6/main.cpp
+++ task2-6/main.cpp
@@ -8,63 +8,25 @@
 #include <iostream>
 #include <set>
 #include <algorithm>
+#include "task2-6.h"
 
 using namespace std;
 
-const int INF = 1000000000;
-
 int main() {
+    Graph a(7,1);
+    a.addEdge(1,2,7);
+    a.addEdge(1,6,14);
+    a.addEdge(1,3,9);
+    a.addEdge(2,3,10);
+    a.addEdge(2,4,15);
+    a.addEdge(3,4,11);
+    a.addEdge(3,6,2);
+    a.addEdge(4,5,6);
+    a.addEdge(5,6,9);
+    a.dijkstra();
+    a.printDijkstra();
+    a.printPath(6);
 
-    int n;
-    n = 7;
-    // чтение n ...
-    vector < vector < pair<int,int> > > g (n);
-    // чтение графа
-    g[1].push_back(make_pair(2,7));
-    g[1].push_back(make_pair(6,14));
-    g[1].push_back(make_pair(3,9));
-    g[2].push_back(make_pair(3,10));
-    g[2].push_back(make_pair(4,15));
-    g[3].push_back(make_pair(4,11));
-    g[3].push_back(make_pair(6,2));
-    g[4].push_back(make_pair(5,6));
-    g[5].push_back(make_pair(6,9));
-
-    int s = 1; // стартовая вершина
-
-    vector<int> d (n, INF),  p (n); //хранит текущую длину d[v] кратчайшего пути из s в v, по умолчанию бесконечность
-                                    // p[v] хранит предка вершины
-    d[s] = 0; //длина пути из s в s равна 0
-    vector<char> u (n); //здесь будем хранить, помечена вершина или ещё нет
-    for (int i=0; i<n; ++i) {
-        int v = -1;
-        for (int j=0; j<n; ++j)
-            if (!u[j] && (v == -1 || d[j] < d[v])) //ищем вершину с минимальной d
-                v = j;
-        if (d[v] == INF) // если длина пути = бексконечности, переходим к следующей итерации
-            break;
-        u[v] = true; // отмечаем как посещенную
-
-        for (size_t j=0; j<g[v].size(); ++j) {
-            int to = g[v][j].first, // вершина, в которой оказались
-                    len = g[v][j].second; // длина текущего ребра
-            if (d[v] + len < d[to]) {
-                d[to] = d[v] + len; // релаксация
-                p[to] = v; // предком вершины t0 является вершина v
-            }
-        }
-    }
-    for(int i=0; i<n-1; i++)
-        cout << "from " << s << " to " << i+1 << " = " << d[i+1] << endl;
-
-    vector<int> path;
-    for (int v=6; v!=s; v=p[v])
-        path.push_back (v);
-    path.push_back (s);
-    reverse(path.begin(), path.end());
-    for (auto it : path)
-        cout << it << ' ';
-    cout << endl;
 
     return 0;
 }
--- task2-6/task2-6.h
+++ task2-6/task2-6.h
@@ -2,7 +2,27 @@
 // Created by apbfor on 12.05.19.
 //
 
-#ifndef TEST_EMPTY_TASK2_6_H
-#define TEST_EMPTY_TASK2_6_H
+#include <iostream>
+#include <vector>
 
-#endif //TEST_EMPTY_TASK2_6_H
+using namespace std;
+
+class Graph {
+private:
+    const int INF = 1000000000;
+    int n = 0; //count of vert.
+    vector < vector < pair<int,int> > > *g; //graph
+    int s = 1; //vert for start
+    vector<int> *d; //stores length of shortest path from s to v;
+    vector<int> *p; //stores parent of vert;
+    vector<char> *u; //stores logical - visited or not
+    vector<int> path;
+public:
+    void addEdge(int from, int to, int weight);
+    void dijkstra();
+    void printDijkstra();
+    Graph(int size, int start = 1);
+    void printPath(int to);
+    vector<int> getPath();
+
+};
